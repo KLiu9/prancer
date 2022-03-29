e5f0f8b30cea69559bf1d484d0814ccdb3147361
Katie Liu <katieliu@mit.edu>
2022-03-28 22:12:43 -0400

    got parsed text from uploaded json file to display

--- static/src/components/Simplification/DragDrop.js
+++ static/src/components/Simplification/DragDrop.js
@@ -1,45 +1,42 @@
 import React, { useState } from "react";
-import axios from 'axios';
+// import axios from 'axios';
 import { FileUploader } from "react-drag-drop-files";
 
 const fileTypes = ["JSON"];
 
-const DragDrop = ({onRefresh}) => {
+const DragDrop = () => {
     const [file, setFile] = useState('');
     const [uploadPercentage, setUploadPercentage] = useState(0);
 
-    const handleChange = (file) => {
-        console.log('handleChange: %s', file);
-        setFile(file);
-        const formData = new FormData();
-        formData.append('file', file);
-        try {
-            const res = axios.post('/upload', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                },
-            
-                onUploadProgress: progressEvent => {
-                    setUploadPercentage(
-                        parseInt(
-                            Math.round((progressEvent.loaded * 100) / progressEvent.total)
-                        )
-                    );
-                }
-            });
-        } catch (err) {
-            if (err.response.status === 500) {
-              console.log('There was a problem with the server');
-            } else {
-              console.log(err.response.data.msg);
-            }
-            setUploadPercentage(0)
+    const [text, setText] = useState();
+
+    let fileReader;
+
+    const handleFileRead = (file) => {
+        let content = fileReader.result;
+        var out = "";
+        let obj = JSON.parse(content);
+        for (let i = 0; i < obj.length; i++) {
+            out += obj[i]["text"] + "\n";
         }
-        onRefresh();
+        setText(out);
+    };
+
+    const handleChange = (file) => {
+        setFile(file);
+        fileReader = new FileReader();
+        const blob = new Blob([file], { type: "application/json" });
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(blob);
     };
 
     return (
-        <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+        <div>
+            <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+            <div>
+                {text && <pre>{text}</pre>}
+            </div>
+        </div>
     );
 }
 
